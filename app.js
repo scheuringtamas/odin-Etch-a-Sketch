a44c88d1db777d028ee2dfbf15d00f6c88d2f0bf
scheuringtamas <sch.tamas5@gmail.com>
2024-08-10 15:28:46 +0200

    Grid resize

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const container = document.querySelector("#grid-container");
-
 function createGrid(size) {
+  const container = document.querySelector("#grid-container");
+  const squareSize = 960 / size;
+  container.innerHTML = "";
+
   for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div");
     div.classList.add("grid-item");
+    div.style.width = `${squareSize}px`;
+    div.style.height = `${squareSize}px`;
 
     div.addEventListener("mouseover", () => {
       div.style.backgroundColor = "black";
@@ -13,6 +17,18 @@ function createGrid(size) {
   }
 }
 
-function changeColor() {}
+function changeSize() {
+  let newSize;
+  do {
+    newSize = parseInt(
+      prompt("Enter the number of squares per side (max 100):"),
+      10
+    );
+  } while (isNaN(newSize) || newSize <= 0 || newSize > 100);
 
-createGrid(257);
+  createGrid(newSize);
+}
+
+createGrid(16);
+
+document.getElementById("resize").addEventListener("click", changeSize);
